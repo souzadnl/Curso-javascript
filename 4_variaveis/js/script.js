57429dff1402cd69704b1e29426cce0ee27bcf0b
Daniel Souza <souzadnl>
2023-10-03 20:29:23 -0300

    Adicionando as atividades ao repositório

---
+++ 4_variaveis/js/script.js
@@ -0,0 +1,9 @@
+var teste = 1;
+
+teste = "Matheus";
+let testando = 1;
+const teste2 = 2;
+
+console.log(testando);
+console.log(teste2);
+console.log(teste);
---
+++ 5_number/js/script.js
@@ -0,0 +1,18 @@
+var numero = 5;
+console.log(numero);
+console.log(typeof numero);
+
+var float = 5.32;
+
+console.log(float)
+console.log(typeof float)
+
+var texto_com_numero = '523';
+
+console.log(texto_com_numero)
+console.log(typeof texto_com_numero)
+
+console.log(NaN);
+console.log (typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
